c41249e1ffb06f1348a05d29d9b41f1ad51f3044
Kond <137844947+devkond@users.noreply.github.com>
2023-06-28 10:09:48 -0300

    Base Structure

---
+++ main.py
@@ -0,0 +1,5 @@
+import tracker
+
+if __name__ == "__main__":
+    data = tracker.search("nome")
+    print(data)
--- tracker.py
+++ tracker.py
@@ -2,39 +2,42 @@
 import requests
 import os
 
-# asking to try the code
-try:
-    print("=" * 21)
-    name = input("Choose an country by their name: ") 
-    os.system("cls")
+def search(name):
+    return "Dados..."
 
-# Verify if theres an input
-    if name:
-        # define url
-        url = f"https://restcountries.com/v3/name/{name}"
-        # get url informations
-        response = requests.get(url)
-        data = response.json()
+if __name__=="__main__":
+    try:
+        print("=" * 21)
+        name = input("Choose an country by their name: ") 
+        os.system("cls")
 
-        if data:
-            # Pick just the first information
-            country = data[0]
+    # Verify if theres an input
+        if name:
+            # define url
+            url = f"https://restcountries.com/v3/name/{name}"
+            # get url informations
+            response = requests.get(url)
+            data = response.json()
 
-            print("=" * 21)
-            print("Country information:")
-            print(f"Name: {country['name']['common']}")
-            print(f"Capital: {country['capital'][0]}")
-            print(f"Population: {country['population']}")
-            print(f"Region: {country['region']}")
-            print(f"Area: {country['area']} km²")
-            print(f"Country Code: {country['cca2']}")
-            print("=" * 21)
+            if data:
+                # Pick just the first information
+                country = data[0]
+
+                print("=" * 21)
+                print("Country information:")
+                print(f"Name: {country['name']['common']}")
+                print(f"Capital: {country['capital'][0]}")
+                print(f"Population: {country['population']}")
+                print(f"Region: {country['region']}")
+                print(f"Area: {country['area']} km²")
+                print(f"Country Code: {country['cca2']}")
+                print("=" * 21)
+            else:
+                # If dont exist no country with input name, then show this
+                print("No country was found with this name.")
         else:
-            # If dont exist no country with input name, then show this
-            print("No country was found with this name.")
-    else:
-        print("Provide an country name.")
+            print("Provide an country name.")
 
-except Exception as ex:
-    # To prevent mistakes
-    print(f"ERROR: {ex}")
+    except Exception as ex:
+        # To prevent mistakes
+        print(f"ERROR: {ex}")
