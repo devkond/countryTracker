4df60173a49556b7c7473e644a9a48ba846d2496
Kond <137844947+devkond@users.noreply.github.com>
2023-06-29 00:59:30 -0300

    Fix error

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from tkinter import Tk
 from gui import CountryTrackerGUI
 
 if __name__ == "__main__":
-    window = Tk()
-    app = CountryTrackerGUI(window)
-    window.mainloop()
+    CountryTrackerGUI()
